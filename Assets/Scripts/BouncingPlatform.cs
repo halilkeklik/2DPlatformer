2ae32be2fe0e2bd9896edd123c17b519e56fbbda
Halil Keklik <33129298+halilkeklik@users.noreply.github.com>
2020-12-17 16:04:28 +0300

    Add Bouncing Platform

---
+++ Assets/Scripts/BouncingPlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncingPlatform : MonoBehaviour
+{
+    public float bounceForce;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<CharacterMovementController>().jumpJorce += bounceForce;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<CharacterMovementController>().jumpJorce -= bounceForce;
+    }
+}
